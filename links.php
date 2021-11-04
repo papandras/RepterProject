49395b2d62688fd46ad1c4ce3d9625e652e34724
farkaspeter14b <farkas.peter.55@gmail.com>
2021-11-04 10:25:26 +0100

    Create links.php

---
+++ links.php
@@ -0,0 +1,63 @@
+<?php
+$style = json_decode(file_get_contents("settings.json"), true);
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+
+<?php
+require("parts/head.php");
+?>
+
+<body>
+    <div class="plane" id="plane"></div>
+    <div class="plane" id="plane2"></div>
+    <div class="container">
+        <header>
+            <!-- Navbar -->
+            <nav class="navbar navbar-expand navbar-light bg-white">
+                <div class="container-fluid" style="position: relative;">
+                                
+                    <div class="collapse navbar-collapse" id="navbarExample01">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" aria-current="page" href="index.php">Kezdőlap</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">Rólunk</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- Navbar -->
+
+            <!-- Background image -->
+            <div class="p-5 text-center bg-image img-fluid" style="
+                background-image: url('https://sm.ign.com/t/ign_hu/blogroll/m/microsoft-/microsoft-flight-simulator-releases-alpha-testers-gameplay-s_4nf8.1280.jpg');
+                height: 400px;
+                background-size: cover;
+              ">
+                <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+                    <div class="d-flex justify-content-center align-items-center h-100">
+                        <div class="text-white">
+                            <h1 class="mb-3">Shenzhen</h1>
+                            <h4 class="mb-3">Járatinformációk</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Background image -->
+        </header>
+
+        <p id="movetxt">Ezen a weblapon kigyűjtöttük a repterekkel kapcsolatos járatinformációkat.</p>
+
+        <div id="load">
+            <?php
+                require("parts/footer.php");
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
