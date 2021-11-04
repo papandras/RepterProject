ffbe1c79c399e05d754e890c599201f8f53035d6
farkaspeter14b <farkas.peter.55@gmail.com>
2021-11-04 10:24:17 +0100

    Update about.php

--- about.php
+++ about.php
@@ -58,6 +58,8 @@ require("parts/head.php");
             ?>
         </div>
     </div>
+    <p>További információkért látogass el az alábbi weboldalakra!</p><br>
+    <p>A reptér weboldala:</p> <a href=""></a>
 </body>
 
 </html>
